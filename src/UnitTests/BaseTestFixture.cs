f26867f85c84e2ab1d97a9a19ab08614742bd163
dnknitro <vova64@gmail.com>
2016-05-08 18:55:18 -0500

    Renamed unit test files

---
+++ src/UnitTests/BaseTestFixture.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using ServiceStack;
+using ServiceStack.Testing;
+
+namespace UnitTests
+{
+	[TestFixture]
+	public class BaseTestFixture
+	{
+		private static ServiceStackHost _appHost;
+
+		[OneTimeSetUp]
+		public void TestFixtureSetUp()
+		{
+			_appHost = new BasicAppHost().Init();
+		}
+
+		[OneTimeTearDown]
+		public void TestFixtureTearDown()
+		{
+			_appHost.Dispose();
+		}
+	}
+}
